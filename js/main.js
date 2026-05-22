c956e840ebd70065bab5dbe881fb6b5f2d5315f8
zBraaad98 <bradleyjrsmith98@hotmail.com>
2026-05-23 00:04:30 +0100

    Fully added pages and links to whats app and phone number

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+const menuBtn = document.getElementById("menu-btn");
+const mobileDropdown = document.getElementById("mobile-dropdown");
+const menuIcon = document.getElementById("menu-icon");
 
+menuBtn.addEventListener("click", () => {
+  mobileDropdown.classList.toggle("hidden");
+
+  if (mobileDropdown.classList.contains("hidden")) {
+    menuIcon.classList.remove("fa-xmark");
+    menuIcon.classList.add("fa-bars");
+  } else {
+    menuIcon.classList.remove("fa-bars");
+    menuIcon.classList.add("fa-xmark");
+  }
+});
+
+document.querySelectorAll("#mobile-dropdown a").forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileDropdown.classList.add("hidden");
+    menuIcon.classList.remove("fa-xmark");
+    menuIcon.classList.add("fa-bars");
+  });
+});
